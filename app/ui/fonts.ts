8c33cb7186d109c4c0ff9c7f8b76c44789d941b5
jungting20 <jungting20@gmail.com>
2023-11-09 17:41:29 +0900

    first

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana, Nanum_Gothic } from "next/font/google";
+
+export const inter = Inter({ subsets: ["greek"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const nanum = Nanum_Gothic({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
